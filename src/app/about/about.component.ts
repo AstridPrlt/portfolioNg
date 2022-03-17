223ffe3f547a89da01cda06e6d2b739fc5d95e2b
AstridPrlt <31550071+AstridPrlt@users.noreply.github.com>
2022-03-17 23:09:08 +0100

    feat: about section with image follow cursor

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, HostListener, Inject, OnInit, Renderer2, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-about',
@@ -7,9 +7,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AboutComponent implements OnInit {
 
-  constructor() { }
+  @ViewChild("avatar") avatar!: ElementRef;
+  @ViewChild("description") description!: ElementRef;
+
+
+
+  constructor(private _renderer: Renderer2) { }
 
   ngOnInit(): void {
   }
 
+  ngAfterViewInit(): void {
+    // this.followCursor();
+    let imageXInitial = this.avatar.nativeElement.getBoundingClientRect().left;
+    let imageYInitial = this.avatar.nativeElement.getBoundingClientRect().top;
+    this.followCursorWithTranslate(imageXInitial, imageYInitial)
+  }
+
+  // followCursor(): void {
+  //   this._renderer.listen(this.description.nativeElement, 'mousemove', e => {
+  //     this._renderer.setStyle(this.avatar.nativeElement, 'top', `${e.pageY}px`);
+  //     this._renderer.setStyle(this.avatar.nativeElement, 'left', `${e.pageX}px`);
+  //   })
+  //   this._renderer.listen(this.description.nativeElement, 'mouseleave', () => {
+  //     this._renderer.setStyle(this.avatar.nativeElement, 'top', '100%');
+  //     this._renderer.setStyle(this.avatar.nativeElement, 'left', '35%')
+  //   })
+  // }
+
+  followCursorWithTranslate(left: number, top: number) {
+    this._renderer.listen(this.description.nativeElement, 'mousemove', e => {
+      this._renderer.setStyle(this.avatar.nativeElement, 'transform', `translate(${ e.pageX - left }px, ${ e.pageY - top }px)`);
+    })
+    this._renderer.listen(this.description.nativeElement, 'mouseleave', () => {
+      this._renderer.setStyle(this.avatar.nativeElement, 'transform', 'translate(0)');
+    })
+  }
 }
