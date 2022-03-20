8b0247bf91505bd0dce5eb93f4c52e64c6714758
AstridPrlt <31550071+AstridPrlt@users.noreply.github.com>
2022-03-20 19:07:16 +0100

    design: about and projects

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -10,37 +10,31 @@ export class AboutComponent implements OnInit {
   @ViewChild("avatar") avatar!: ElementRef;
   @ViewChild("description") description!: ElementRef;
 
-
-
   constructor(private _renderer: Renderer2) { }
 
   ngOnInit(): void {
   }
 
   ngAfterViewInit(): void {
-    // this.followCursor();
     let imageXInitial = this.avatar.nativeElement.getBoundingClientRect().left;
     let imageYInitial = this.avatar.nativeElement.getBoundingClientRect().top;
     this.followCursorWithTranslate(imageXInitial, imageYInitial)
   }
 
-  // followCursor(): void {
-  //   this._renderer.listen(this.description.nativeElement, 'mousemove', e => {
-  //     this._renderer.setStyle(this.avatar.nativeElement, 'top', `${e.pageY}px`);
-  //     this._renderer.setStyle(this.avatar.nativeElement, 'left', `${e.pageX}px`);
-  //   })
-  //   this._renderer.listen(this.description.nativeElement, 'mouseleave', () => {
-  //     this._renderer.setStyle(this.avatar.nativeElement, 'top', '100%');
-  //     this._renderer.setStyle(this.avatar.nativeElement, 'left', '35%')
-  //   })
-  // }
-
   followCursorWithTranslate(left: number, top: number) {
+
     this._renderer.listen(this.description.nativeElement, 'mousemove', e => {
-      this._renderer.setStyle(this.avatar.nativeElement, 'transform', `translate(${ e.pageX - left }px, ${ e.pageY - top }px)`);
-    })
-    this._renderer.listen(this.description.nativeElement, 'mouseleave', () => {
-      this._renderer.setStyle(this.avatar.nativeElement, 'transform', 'translate(0)');
-    })
-  }
+      let descriptionPosition = this.description.nativeElement.getBoundingClientRect();
+      console.log(descriptionPosition);
+
+        if(descriptionPosition.bottom > (descriptionPosition.height / 2)) {
+        this._renderer.setStyle(this.avatar.nativeElement, 'transform', `translate(${ e.pageX - left }px, ${ e.pageY - top }px)`);
+        this._renderer.setStyle(this.avatar.nativeElement, 'opacity', 0.5);
+      }
+      })
+      this._renderer.listen(this.description.nativeElement, 'mouseleave', () => {
+        this._renderer.setStyle(this.avatar.nativeElement, 'transform', 'none');
+        this._renderer.setStyle(this.avatar.nativeElement, 'opacity', 1);
+      })
+    }
 }
--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, HostListener, Inject, OnInit, Renderer2, ViewChild } from '@angular/core';
+import { Component, Inject, OnInit, Renderer2 } from '@angular/core';
 import { ProjectItem } from '../_models/project-item.model';
 import ProjectsJson from '../../assets/projectsJson.json';
 import { DOCUMENT } from '@angular/common';
@@ -11,84 +11,83 @@ import { DOCUMENT } from '@angular/common';
 export class ProjectsComponent implements OnInit {
   list: ProjectItem[] = ProjectsJson;
 
-  @ViewChild("listOfProjects") listOfProjects!: ElementRef;
-  @ViewChild("growingPart") growingPart!: ElementRef;
-  @ViewChild("fixePart") fixePart!: ElementRef;
+  // @ViewChild("listOfProjects") listOfProjects!: ElementRef;
+  // @ViewChild("growingPart") growingPart!: ElementRef;
+  // @ViewChild("fixePart") fixePart!: ElementRef;
 
-  listPositionLeft!: number;
-  listPositionRight!: number;
-  navScroll: number = 200;
+  // listPositionLeft!: number;
+  // listPositionRight!: number;
+  // navScroll: number = 200;
 
-  widthProgressionBar!: number;
-  listWidth!: number;
-  barWidth!: number;
+  // widthProgressionBar!: number;
+  // listWidth!: number;
+  // barWidth!: number;
 
-  @HostListener('mousemove', ['$event']) onMousemove(event: MouseEvent): void {
-    console.log(event);
-
-  }
+  // @HostListener('mousemove', ['$event']) onMousemove(event: MouseEvent): void {
+  //   console.log(event);
+  // }
 
   constructor(private _renderer: Renderer2, @Inject(DOCUMENT) private document: Document) { }
 
   ngOnInit(): void {
-    console.log(this.list);
+    // console.log(this.list);
 
   }
 
-  ngAfterViewInit(): void {
-    this.listPositionLeft = this.listOfProjects.nativeElement.getBoundingClientRect().left;
-    this.listPositionRight = this.listOfProjects.nativeElement.getBoundingClientRect().right;
-    this.listWidth = this.listOfProjects.nativeElement.getBoundingClientRect().width;
-    this.barWidth = this.fixePart.nativeElement.getBoundingClientRect().width;
+  // ngAfterViewInit(): void {
+    // this.listPositionLeft = this.listOfProjects.nativeElement.getBoundingClientRect().left;
+    // this.listPositionRight = this.listOfProjects.nativeElement.getBoundingClientRect().right;
+    // this.listWidth = this.listOfProjects.nativeElement.getBoundingClientRect().width;
+    // this.barWidth = this.fixePart.nativeElement.getBoundingClientRect().width;
 
-    this.widthProgressionBar = 1 - ((this.listPositionRight - this.document.body.clientWidth)/ this.listWidth);
-    this._renderer.setStyle(this.growingPart.nativeElement, 'width', `${this.widthProgressionBar*100}%`);
-  }
+    // this.widthProgressionBar = 1 - ((this.listPositionRight - this.document.body.clientWidth)/ this.listWidth);
+    // this._renderer.setStyle(this.growingPart.nativeElement, 'width', `${this.widthProgressionBar*100}%`);
+  // }
 
-  navNext(): void {
-    this.listPositionRight = this.listOfProjects.nativeElement.getBoundingClientRect().right;
-    if(this.listPositionRight > this.document.body.clientWidth*0.95) {
-        let distanceToMove;
-        if(this.listPositionRight - this.navScroll < this.document.body.clientWidth*0.95 ) {
-          distanceToMove = this.listPositionLeft - (this.listPositionRight - this.document.body.clientWidth*0.95);
-        } else {
-          distanceToMove = this.listPositionLeft - this.navScroll;
-        }
+//   navNext(): void {
+//     this.listPositionRight = this.listOfProjects.nativeElement.getBoundingClientRect().right;
+//     if(this.listPositionRight > this.document.body.clientWidth*0.95) {
+//         let distanceToMove;
+//         if(this.listPositionRight - this.navScroll < this.document.body.clientWidth*0.95 ) {
+//           distanceToMove = this.listPositionLeft - (this.listPositionRight - this.document.body.clientWidth*0.95);
+//         } else {
+//           distanceToMove = this.listPositionLeft - this.navScroll;
+//         }
 
-      if(distanceToMove < -(this.listWidth - this.listPositionRight)) {
-      this._renderer.setStyle(this.listOfProjects.nativeElement, 'transform', `translateX(${distanceToMove}px)`);
-      this.listPositionLeft -= this.navScroll;
-      this.setProgressionBarTranslate();
-      }
+//       if(distanceToMove < -(this.listWidth - this.listPositionRight)) {
+//       this._renderer.setStyle(this.listOfProjects.nativeElement, 'transform', `translateX(${distanceToMove}px)`);
+//       this.listPositionLeft -= this.navScroll;
+//       this.setProgressionBarTranslate();
+//       }
 
-      console.log(distanceToMove, this.listOfProjects.nativeElement.getBoundingClientRect());
-  }
-}
-  navBack(): void {
-    this.listPositionRight = this.listOfProjects.nativeElement.getBoundingClientRect().right;
-    this.listPositionLeft = this.listOfProjects.nativeElement.getBoundingClientRect().left;
-    if(this.listPositionLeft < 0) {
-      let distanceToMove;
-      if(this.listPositionLeft > -(this.navScroll)) {
-        distanceToMove = 0;
-      } else {
-        distanceToMove = this.listPositionLeft + this.navScroll;
-      }
-      this._renderer.setStyle(this.listOfProjects.nativeElement, 'transform', `translateX(${distanceToMove}px)`);
-      this.setProgressionBarTranslate();
+//       console.log(distanceToMove, this.listOfProjects.nativeElement.getBoundingClientRect());
+//   }
+// }
+//   navBack(): void {
+//     this.listPositionRight = this.listOfProjects.nativeElement.getBoundingClientRect().right;
+//     this.listPositionLeft = this.listOfProjects.nativeElement.getBoundingClientRect().left;
+//     if(this.listPositionLeft < 0) {
+//       let distanceToMove;
+//       if(this.listPositionLeft > -(this.navScroll)) {
+//         distanceToMove = 0;
+//       } else {
+//         distanceToMove = this.listPositionLeft + this.navScroll;
+//       }
+//       this._renderer.setStyle(this.listOfProjects.nativeElement, 'transform', `translateX(${distanceToMove}px)`);
+//       this.setProgressionBarTranslate();
 
-      console.log(this.listOfProjects.nativeElement.getBoundingClientRect());
-    }
-  }
+//       console.log(this.listOfProjects.nativeElement.getBoundingClientRect());
+//     }
+//   }
 
-  setProgressionBarTranslate(): void {
-    console.log(this.widthProgressionBar);
-    let widthProgressionBarCalculation = 1 - ((this.listPositionRight - this.navScroll - this.document.body.clientWidth)/this.listOfProjects.nativeElement.getBoundingClientRect().width);
-    this.widthProgressionBar = widthProgressionBarCalculation > 1 ? 1 : widthProgressionBarCalculation;
-    this._renderer.setStyle(this.growingPart.nativeElement, 'width', `${this.widthProgressionBar*100}%`);
+//   setProgressionBarTranslate(): void {
+//     console.log(this.widthProgressionBar);
+//     let widthProgressionBarCalculation = 1 - ((this.listPositionRight - this.navScroll - this.document.body.clientWidth)/this.listOfProjects.nativeElement.getBoundingClientRect().width);
+//     this.widthProgressionBar = widthProgressionBarCalculation > 1 ? 1 : widthProgressionBarCalculation;
+//     this._renderer.setStyle(this.growingPart.nativeElement, 'width', `${this.widthProgressionBar*100}%`);
 
-    console.log(this.widthProgressionBar);
+//     console.log(this.widthProgressionBar);
 
-  }
+//   }
 
 }
